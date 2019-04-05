d93856aa494591e38d2e2bed27c621b6b9524459
Caroline Watson <caroline.watson@duke.edu>
2019-04-05 09:07:01 -0400

    Update to data exploration

--- Data/Code/Watson_Final_Code.R
+++ Data/Code/Watson_Final_Code.R
@@ -25,7 +25,7 @@ theme_set(caroline_theme)
 
 #This section of code looks at importing my dataset and summarizing it
 #reading in data file
-carbon.data <- read.csv("./Data/Raw/NTL-LTER_Lake_Carbon_Raw.csv")
+carbon.data <- read.csv("./Data/Raw/NTL-LTER_Lake_Carbon_Raw.csv", stringsAsFactors = F)
 
 #structure of data frame
 carbon.data_summary <- summary(carbon.data)
@@ -76,5 +76,16 @@ ggplot(carbon.data) +
                  y = depth.id))
 
 #split depth column up into categorical column and continuous column to do data analysis
+carbon.data.separate <- separate(carbon.data, depth, c("Lake.Layer", "depth"), "\\|")
+                                 
+carbon.data.separate2 <- separate(carbon.data, depth, c("Hypolimnion", "Metalimnion", "PML", "Epilimnion"))
+
+#, sep = (?<=[a-z]))
+
+#make a new column with seasons that are seperated by daynum; then write a pipe function saying if daynum == [1:150], "summer" etc
+carbon.data.separate$season <- NA
+
 #divide the daynum into seasons, run an anova on this to see if season is a random effect
+
+
 #make a graph with facet wrap for doc and season; facet wrap by season
